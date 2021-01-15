b0d5584fdf61470101f3c3ae8fe5ce86dc6b59c9
KerimAksak <abdulkerim.aksak@outlook.com>
2021-01-14 19:41:51 -0500

    çekilecek veri tipi csv formatına çevrildi

--- getData.py
+++ getData.py
@@ -1,30 +1,28 @@
-import os
-import tweepy as tw
-import pandas as pd
-
-consumer_key= 'consumer_key'
-consumer_secret= 'consumer_secret'
-access_token= 'access_token'
-access_token_secret= 'access_token_secret'
-
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
+#python libraries
+import datetime
+import tweepy
+import csv
+#Keys and access for Twitter
+consumer_key = 'consumer_key'
+consumer_secret = 'consumer_secret'
+access_token = 'access_token'
+access_token_secret = 'access_token_secret'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth)
 
-# Define the search term and the date_since date as variables
-search_words = "#aşı"
-date_since = "2020-11-01"
+filename = 'twitter_csvData'+'.csv' 
 
-# Collect tweets
-tweets = tw.Cursor(api.search,
-              q=search_words,
-              lang="tr",
-              since=date_since).items(300000)
+date_since = "tw_tarihi"
+user_name = "tw_kullanıcı_ismi"
+search_words = "aranacak_kelime"
 
-f = open("dataset.txt", "a")
-
-for tweet in tweets:
-    print(tweet.text)
-    f.write(tweet.text)
-
-f.close()
+tweets_data = []
+with open (filename, 'a+', newline='') as csvFile:
+    csvWriter = csv.writer(csvFile)
+    for tweet in tweepy.Cursor(api.user_timeline, id=user_name, q = search_words, lang = "tr", since=all, count =1).items(1): 
+        tweets_encoded = tweet.text.encode('utf-8')
+        tweets_decoded = tweets_encoded.decode('utf-8')
+        #parsed_tweet = api.get_status(tweet.id, tweet_mode='extended').full_text
+        csvWriter.writerow([date_since, tweet.user.screen_name, tweet.user.name, tweet.text, tweet.retweet_count, tweet.favorite_count])
+        #Direkt olarak veriler csv formatından kaydediliyor.
